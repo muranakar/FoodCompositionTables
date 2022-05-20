9715fafa279a70d71bd033b6685e59cb94785b91
toaster08 <y10star.hon@gmail.com>
2022-05-20 18:53:14 +0900

    値オブジェクトとしてSection型を作成

--- FoodCompositRealmSample/ViewController/FoodListViewController.swift
+++ FoodCompositRealmSample/ViewController/FoodListViewController.swift
@@ -11,10 +11,16 @@ protocol FoodListViewTransitonDelegate: AnyObject {
     func transitPresentingVC(_: () -> Void)
 }
 
+//ValueObject
 struct Section {
-    let number:Int
-    init(at index: Int) {
-        self.number = index
+    let number: Int
+    //IndexPath型を渡した場合
+    init(at indexPath: IndexPath) {
+        self.number = indexPath.section
+    }
+    //sectionを直接渡した場合
+    init(_ section: Int) {
+        self.number = section
     }
 }
 
@@ -25,13 +31,15 @@ class FoodListViewController: UIViewController,FoodRegistrationDelegate {
     private var selectingFood: FoodObject?
     private var foodList: [FoodObject] = []
     private var searchedFoodList: [FoodObject] = []
-    private var searchController = UISearchController()
     
+    @IBOutlet private weak var foodSearchBar: UISearchBar!
     @IBOutlet private weak var tableView: UITableView!
     
     //検索時のTableViewを覆い隠す用
     @IBOutlet private weak var coverView: UIView!
     @IBOutlet private weak var contentsCoverView: UIView!
+    
+    @IBOutlet weak var navSettigButton: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,9 +47,9 @@ class FoodListViewController: UIViewController,FoodRegistrationDelegate {
                            forCellReuseIdentifier: "Cell")
         tableView.delegate = self
         tableView.dataSource = self
-        
-        setupSearchBar()
+//        setupSearchBar()
         configure()
+        configureSearchBar()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -55,26 +63,26 @@ class FoodListViewController: UIViewController,FoodRegistrationDelegate {
         foodList = FoodCompositionTableUseCase().foodTable
     }
     
-    private func setupSearchBar() {
-        navigationItem.searchController = searchController
-        navigationItem.searchController?.searchResultsUpdater = self
-        navigationItem.searchController?.hidesNavigationBarDuringPresentation = false
-        navigationItem.hidesSearchBarWhenScrolling = false
+    private func configureSearchBar() {
+        foodSearchBar.delegate = self
+        foodSearchBar.showsCancelButton = false
+        foodSearchBar.showsSearchResultsButton = true
     }
     
     func transitPresentingVC() {
-        searchController.isActive = false
         delegate?.transitPresentingVC {
             print(String(describing: delegate?.transitPresentingVC))
         }
         self.dismiss(animated: true)
     }
     
-    func arrange(_ foods: [FoodObject], in categorySection: Section) -> [FoodObject] {
+    func arrangeCell(of foods: [FoodObject], in categorySection: Section) -> [FoodObject] {
         
-        for case CategoryType.allCases[categorySection.number] in CategoryType.allCases {
+        let sectionCategory = CategoryType.allCases[categorySection.number]
+        
+        for case sectionCategory in CategoryType.allCases {
             let filterdFoods = foods.filter {
-                $0.category == CategoryType.allCases[categorySection.number].name
+                $0.category == sectionCategory.name
             }
             return filterdFoods
         }
@@ -84,21 +92,22 @@ class FoodListViewController: UIViewController,FoodRegistrationDelegate {
     
     //indexPath.rowとsectionの使い分けが同じ中に入っているのが問題
     func searchedResultFoods(between section: Section) -> [FoodObject] {
-        let resultedFoods: [FoodObject]
         
-        if searchController.isActive {
-            resultedFoods = arrange(searchedFoodList, in: section)
+        let resultedFoods: [FoodObject]
+        if foodSearchBar.searchTextField.isEditing {
+            resultedFoods = arrangeCell(of: searchedFoodList, in: section)
         } else {
-            resultedFoods = arrange(foodList, in: section)
+            resultedFoods = arrangeCell(of: foodList, in: section)
         }
+        
         return resultedFoods
     }
 }
 
-extension FoodListViewController: UITableViewDelegate {
+extension FoodListViewController: UITableViewDelegate, UITableViewDataSource {
     // 各セルを選択した時に実行されるメソッド
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let searchedFoods = searchedResultFoods(between: Section(at: indexPath.section))
+        let searchedFoods = searchedResultFoods(between: Section(at: indexPath))
         self.selectingFood = searchedFoods[indexPath.row]
         performSegue(withIdentifier: "toFoodCompositionVC", sender: nil)
     }
@@ -111,12 +120,10 @@ extension FoodListViewController: UITableViewDelegate {
         //CategoryTypeによるsectionの区別
         return CategoryType.allCases[section].name
     }
-}
 
-extension FoodListViewController: UITableViewDataSource {
     // データの数（＝セルの数）を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let searchedResultFoods = searchedResultFoods(between: Section(at: section))
+        let searchedResultFoods = searchedResultFoods(between: Section(section))
         return searchedResultFoods.count
     }
     // 各セルの内容を返すメソッド
@@ -124,7 +131,7 @@ extension FoodListViewController: UITableViewDataSource {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { fatalError() }
         //ここはindex.sectionにしなければいけないのが分かりづらい
-        let searchedResultFoods = searchedResultFoods(between: Section(at: indexPath.section))
+        let searchedResultFoods = searchedResultFoods(between: Section(at: indexPath))
         let foodName = searchedResultFoods[indexPath.row].foodName
         
         var content = UIListContentConfiguration.cell()
@@ -137,15 +144,20 @@ extension FoodListViewController: UITableViewDataSource {
 
 extension FoodListViewController: UISearchBarDelegate {
     
-    
-}
-
-extension FoodListViewController: UISearchResultsUpdating {
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let searchBarText = searchController.searchBar.text else { return }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        guard let searchBarText = searchBar.searchTextField.text else { return }
         searchedFoodList = search(for: searchBarText, in: foodList)
         tableView.reloadData()
+        searchBar.searchTextField.endEditing(true)
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        tableView.isUserInteractionEnabled = true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        tableView.isUserInteractionEnabled = true
     }
     
     func search(for text: String, in foods: [FoodObject]) -> [FoodObject] {
@@ -154,5 +166,10 @@ extension FoodListViewController: UISearchResultsUpdating {
             return searchedFood
         }
         return searchedFoods
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchedFoodList = search(for: searchText, in: foodList)
+        tableView.reloadData()
     }
 }
