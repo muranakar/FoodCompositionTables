b7c6ad90093da9d91d78aaa0e520f8c21d905884
toaster08 <y10star.hon@gmail.com>
2022-05-20 22:31:52 +0900

    Viewのレイアウト修正

--- FoodCompositRealmSample/ViewController/FoodCompositionViewController.swift
+++ FoodCompositRealmSample/ViewController/FoodCompositionViewController.swift
@@ -19,6 +19,7 @@ final class FoodCompositionViewController: UIViewController, UITableViewDelegate
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet private weak var registerFoodButton: UIButton!
     @IBOutlet private weak var foodWeightTextField: UITextField!
+    @IBOutlet private weak var foodWeightRegisterView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +27,7 @@ final class FoodCompositionViewController: UIViewController, UITableViewDelegate
                            forCellReuseIdentifier: "CompositionCell")
         tableView.delegate = self
         tableView.dataSource = self
+        setup()
         
         registerFoodButton
             .addTarget(self,
@@ -33,27 +35,37 @@ final class FoodCompositionViewController: UIViewController, UITableViewDelegate
                        for: .touchUpInside)
     }
     
+    private func setup() {
+        foodWeightRegisterView.layer.cornerRadius = 20
+        foodWeightRegisterView.layer.borderColor = UIColor.white.cgColor
+        foodWeightRegisterView.layer.borderWidth = 0.5
+        foodWeightRegisterView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        foodWeightRegisterView.layer.shadowColor = UIColor.black.cgColor
+        foodWeightRegisterView.layer.shadowOpacity = 0.5
+    }
+    
     @objc private func registerFoodTouchUpInside() {
         guard let selectFood = selectFood else { return }
         guard let selectFoodWeight
                 = Double(foodWeightTextField.text ?? "") else { return }
        
-        let selectFoodObject = SelectFoodObject(food: selectFood,
-                                          weight: selectFoodWeight)
+        guard let selectFoodObject = SelectFoodObject(food: selectFood,
+                                                      weight: selectFoodWeight) else { return }
+        
         SelectFoodsTableUseCase().save(selectFood: selectFoodObject)
         delegate?.transitPresentingVC()
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        CompositionType.allCases.count
+        FoodCompositionType.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CompositionCell") else { fatalError() }
         guard let selectFood = selectFood else { return cell }
         //栄養素名称
-        let composition = CompositionType.allCases[indexPath.row]
+        let composition = FoodCompositionType.allCases[indexPath.row]
         let compositionName = composition.nameString
         //栄養素量
         let compositionValueString = composition.valueString(in: selectFood)
